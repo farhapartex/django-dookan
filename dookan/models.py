1514283c6acbdfc7a00d9d1353b2339bd8dee308
Md Nazmul Hasan <farhapartex@gmail.com>
2020-04-20 00:01:38 +0600

    system app added and Base , Customer model transfered to system app

--- dookan/models.py
+++ dookan/models.py
@@ -5,58 +5,18 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.forms import ValidationError
 from django.utils.text import slugify
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 import logging, sys, uuid
-from .utils import *
+from system.models import Base, Customer
+from system.utils import *
 from .files import *
 from .media import DynamicImageResize
 
 
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
 # Create your models here.
 logger = logging.getLogger(__name__)
 USER_MODEL = get_user_model()
-
-"""
-Base model is abstract. It is extended by all other models for some default information such as created_at,
-created_by, updated_at, updated_by
-
-"""
-
-class Base(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(USER_MODEL, null=True, editable=False,
-                                   related_name="%(app_label)s_%(class)s_created", on_delete=models.CASCADE)
-    updated_at = models.DateTimeField(auto_now=True)
-    updated_by = models.ForeignKey(USER_MODEL, null=True, editable=False,
-                                   related_name="%(app_label)s_%(class)s_updated", on_delete=models.CASCADE)
-    
-
-    def save(self, *args, **kwargs):
-        user = get_current_user()
-        if user and user.is_authenticated:
-            self.updated_by = user
-            if self._state.adding:
-                self.created_by = user
-        super(Base, self).save(*args, **kwargs)
-
-    class Meta:
-        abstract = True
-
-
-class System(Base):
-    name = models.CharField(_("System Name"), max_length=50)
-    logo = models.ForeignKey("dookan.Media", verbose_name=_("System Logo"), related_name="logos", on_delete=models.SET_NULL, blank=True, null=True)
-
-    def clean(self):
-        if not self.id:
-            if System.objects.all().count()>0:
-                raise ValidationError(_("More than one site information can't be created"), code='invalid')
-
-    def __str__(self):
-        return self.name
-
 
 class Media(Base):
     title = models.CharField(_("Image Title"), max_length=50, blank=True, null=True)
@@ -83,26 +43,6 @@ class Media(Base):
     
     def __str__(self):
         return self.title
-
-
-
-class Customer(Base):
-    """docstring for Customer."""
-    user = models.OneToOneField(USER_MODEL, verbose_name=_("User"), related_name="customer", on_delete=models.CASCADE)
-    mobile = models.CharField(_("Mobile Number"), null=True, max_length=20)
-    billing_address = models.TextField(_("Shipping Address"))
-    same_address = models.BooleanField(_("Same address for delivery?"))
-    delivery_address = models.TextField(_("Delivery Address"), blank=True, null=True)
-    active = models.BooleanField(_("ACtive"), default=True)
-
-    def save(self, *args, **kwargs):
-        if self.same_address is True or self.delivery_address is None:
-            self.delivery_address = self.billing_address
-
-        return super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.user.username
 
 
 class PaymentMethod(Base):
---
+++ system/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from dookan.widgets import *
+from .models import *
+
+# Register your models here.
+
+class CustomerAdminForm(forms.ModelForm):
+    model = Customer
+    class Meta:
+        fields = '__all__'
+        widgets = {
+            'billing_address': HtmlEditor(attrs={'style': 'width: 90%; height: 100%;'}),
+            'delivery_address': HtmlEditor(attrs={'style': 'width: 90%; height: 100%;'}),
+        }
+
+@admin.register(System)
+class SystemAdmin(admin.ModelAdmin):
+    list_display = ("name", "logo", "created_by", "created_at")
+    autocomplete_fields = ['logo']
+    
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    form = CustomerAdminForm
+    list_display = ("user", "mobile", "active", "created_by", "action")
+    list_display_links = ('action',)
+    list_filter = ('created_at', )
+    search_fields = ['user__username',]
+    fields = (("user", "mobile", "active"),  "billing_address","same_address", "delivery_address")
+    list_per_page=10
+
+    def action(self, obj):
+        return format_html('{}'.format('Edit'))
