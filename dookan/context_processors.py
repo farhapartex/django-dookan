70b5edc62034e0a2c2176208620171f3335e8394
Md Nazmul Hasan <farhapartex@gmail.com>
2020-04-20 11:18:45 +0600

    recent order list added to custom context processor and added to dashboard

--- dookan/context_processors.py
+++ dookan/context_processors.py
@@ -1,3 +1,4 @@
+import datetime
 from dookan.models import *
 from system.models import *
 
@@ -17,10 +18,29 @@ def get_customer_info():
     except:
         return 0
 
+def get_total_order_today():
+    today = datetime.date.today()
+    try:
+        order_queryset = Order.objects.all()
+        total_orders = order_queryset.filter(created_at__date=today).count()
+        return total_orders
+    except:
+        return 0
+
+def get_recent_orders():
+    try:
+        order_queryset = Order.objects.all()
+        return order_queryset if order_queryset.count() > 10 else order_queryset[:10]
+    except:
+        return []
+
+
 def summary_data(request):
     
     return {
         "pending_orders": get_order_info(),
         "total_customers": get_customer_info(),
+        "total_order_today": get_total_order_today(),
+        "recent_orders": get_recent_orders(),
     }
     
